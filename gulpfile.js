e52a513d76c6b0d816a5a25de78e67745cf84dd3
ellq <lastquincy13@gmail.com>
2020-05-25 18:30:48 +0500

    Исправление ошибок в проекте

--- gulpfile.js
+++ gulpfile.js
@@ -96,6 +96,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.scss", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("copy", "refresh"));
 });
 
 gulp.task("build", gulp.series(
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,20 +1,21 @@
-var navToggle = document.querySelector(".main-nav__toggle");
+var navToggle = document.querySelector(".main-nav-toggle");
 var nav = document.querySelector(".main-nav");
 
+navToggle.classList.remove("main-nav-toggle--nojs");
 nav.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", function () {
   if (nav.classList.contains("main-nav--closed")) {
     nav.classList.remove("main-nav--closed");
     nav.classList.add("main-nav--opened");
 
-    navToggle.classList.remove("main-nav__toggle--closed");
-    navToggle.classList.add("main-nav__toggle--opened");
+    navToggle.classList.remove("main-nav-toggle--closed");
+    navToggle.classList.add("main-nav-toggle--opened");
   } else {
     nav.classList.remove("main-nav--opened");
     nav.classList.add("main-nav--closed");
 
-    navToggle.classList.remove("main-nav__toggle--opened");
-    navToggle.classList.add("main-nav__toggle--closed");
+    navToggle.classList.remove("main-nav-toggle--opened");
+    navToggle.classList.add("main-nav-toggle--closed");
   }
 })
